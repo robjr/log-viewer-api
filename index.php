15fc9472e7ef65898bc7c1269b277c955243992f
Roberto Júnior <me@robertojunior.net>
2017-07-16 18:22:12 -0300

    Create application

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
+use Controller\LogController;
+
 require 'vendor/autoload.php';
+require 'config/prod.php';
+
 $app = new Silex\Application();
+
+$app->post('logs/readings/forwards', LogController::class . '::readLogForward')
+    ->assert('filePath', '.+');
+
+$app->post('logs/readings/backwards', LogController::class . '::readLogBackward')
+    ->assert('filePath', '.+');
 
 $app->run();
